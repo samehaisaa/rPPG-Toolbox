8e432a72b1b70e6a59272170933797b59e8879a7
Sameh <39239860+samehaisaa@users.noreply.github.com>
2025-03-24 08:07:58 +0000

    Update PhysFormerTrainer.py

--- neural_methods/trainer/PhysFormerTrainer.py
+++ neural_methods/trainer/PhysFormerTrainer.py
@@ -201,64 +201,64 @@ class PhysFormerTrainer(BaseTrainer):
             RMSE = np.mean([(i-j)**2 for i, j in hrs])**0.5
         return RMSE
 
-def test(self, data_loader):
-    """Runs the model on test sets."""
-    if data_loader["test"] is None:
-        raise ValueError("No data for test")
+    def test(self, data_loader):
+        """Runs the model on test sets."""
+        if data_loader["test"] is None:
+            raise ValueError("No data for test")
+        
+        print('')
+        print("===Testing===")
     
-    print('')
-    print("===Testing===")
-
-    # Change chunk length to be test chunk length
-    self.chunk_len = self.config.TEST.DATA.PREPROCESS.CHUNK_LENGTH
-
-    predictions = dict()
-    uncertainties = dict()  # dictionary for uncertainty estimates
-    labels = dict()
-
-    if self.config.TOOLBOX_MODE == "only_test":
-        if not os.path.exists(self.config.INFERENCE.MODEL_PATH):
-            raise ValueError("Inference model path error! Please check INFERENCE.MODEL_PATH in your yaml.")
-        self.model.load_state_dict(torch.load(self.config.INFERENCE.MODEL_PATH))
-        print("Testing uses pretrained model!")
-        print(self.config.INFERENCE.MODEL_PATH)
-    else:
-        if self.config.TEST.USE_LAST_EPOCH:
-            last_epoch_model_path = os.path.join(
-                self.model_dir, self.model_file_name + '_Epoch' + str(self.max_epoch_num - 1) + '.pth')
-            print("Testing uses last epoch as non-pretrained model!")
-            print(last_epoch_model_path)
-            self.model.load_state_dict(torch.load(last_epoch_model_path))
+        # Change chunk length to be test chunk length
+        self.chunk_len = self.config.TEST.DATA.PREPROCESS.CHUNK_LENGTH
+    
+        predictions = dict()
+        uncertainties = dict()  # dictionary for uncertainty estimates
+        labels = dict()
+    
+        if self.config.TOOLBOX_MODE == "only_test":
+            if not os.path.exists(self.config.INFERENCE.MODEL_PATH):
+                raise ValueError("Inference model path error! Please check INFERENCE.MODEL_PATH in your yaml.")
+            self.model.load_state_dict(torch.load(self.config.INFERENCE.MODEL_PATH))
+            print("Testing uses pretrained model!")
+            print(self.config.INFERENCE.MODEL_PATH)
         else:
-            best_model_path = os.path.join(
-                self.model_dir, self.model_file_name + '_Epoch' + str(self.best_epoch) + '.pth')
-            print("Testing uses best epoch selected using model selection as non-pretrained model!")
-            print(best_model_path)
-            self.model.load_state_dict(torch.load(best_model_path))
-
-    self.model = self.model.to(self.config.DEVICE)
-    self.model.eval()
-    print("Running model evaluation on the testing dataset!")
-
-    with torch.no_grad():
-        for _, test_batch in enumerate(tqdm(data_loader["test"], ncols=80)):
-            batch_size = test_batch[0].shape[0]
-            data = test_batch[0].to(self.config.DEVICE)
-            label = test_batch[1].to(self.config.DEVICE)
-            gra_sharp = 2.0
-
-            # Use the new predict_with_uncertainty method
-            mean_ppg_test, std_ppg_test, _ = self.model.predict_with_uncertainty(data, gra_sharp, num_samples=20)
-            for idx in range(batch_size):
-                subj_index = test_batch[2][idx]
-                sort_index = int(test_batch[3][idx])
-                if subj_index not in predictions.keys():
-                    predictions[subj_index] = dict()
-                    uncertainties[subj_index] = dict()
-                    labels[subj_index] = dict()
-                predictions[subj_index][sort_index] = mean_ppg_test[idx].cpu().numpy()
-                uncertainties[subj_index][sort_index] = std_ppg_test[idx].cpu().numpy()
-                labels[subj_index][sort_index] = label[idx].cpu().numpy()
+            if self.config.TEST.USE_LAST_EPOCH:
+                last_epoch_model_path = os.path.join(
+                    self.model_dir, self.model_file_name + '_Epoch' + str(self.max_epoch_num - 1) + '.pth')
+                print("Testing uses last epoch as non-pretrained model!")
+                print(last_epoch_model_path)
+                self.model.load_state_dict(torch.load(last_epoch_model_path))
+            else:
+                best_model_path = os.path.join(
+                    self.model_dir, self.model_file_name + '_Epoch' + str(self.best_epoch) + '.pth')
+                print("Testing uses best epoch selected using model selection as non-pretrained model!")
+                print(best_model_path)
+                self.model.load_state_dict(torch.load(best_model_path))
+    
+        self.model = self.model.to(self.config.DEVICE)
+        self.model.eval()
+        print("Running model evaluation on the testing dataset!")
+    
+        with torch.no_grad():
+            for _, test_batch in enumerate(tqdm(data_loader["test"], ncols=80)):
+                batch_size = test_batch[0].shape[0]
+                data = test_batch[0].to(self.config.DEVICE)
+                label = test_batch[1].to(self.config.DEVICE)
+                gra_sharp = 2.0
+    
+                # Use the new predict_with_uncertainty method
+                mean_ppg_test, std_ppg_test, _ = self.model.predict_with_uncertainty(data, gra_sharp, num_samples=20)
+                for idx in range(batch_size):
+                    subj_index = test_batch[2][idx]
+                    sort_index = int(test_batch[3][idx])
+                    if subj_index not in predictions.keys():
+                        predictions[subj_index] = dict()
+                        uncertainties[subj_index] = dict()
+                        labels[subj_index] = dict()
+                    predictions[subj_index][sort_index] = mean_ppg_test[idx].cpu().numpy()
+                    uncertainties[subj_index][sort_index] = std_ppg_test[idx].cpu().numpy()
+                    labels[subj_index][sort_index] = label[idx].cpu().numpy()
 
     print('')
     calculate_metrics(predictions, labels, self.config)
